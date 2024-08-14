8443a301283b1bb71235b176be927fa41926338e
rafa barruffe <rbarruffe@gmail.com>
2024-08-13 23:29:53 -0300

    Print to variables

---
+++ main.dart
@@ -0,0 +1,16 @@
+var name = 'voyager';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyobjects = ['jupiter', 'saturn', 'uranus', 'neptune'];
+var image = {
+    'tags' : ['saturn'],
+    'url' : '//path/to/saturn.jpg'
+};
+
+void main(){
+    print('I barely know what is ' + name);
+    print(year);
+    print(antennaDiameter);
+    print(flybyobjects);
+    print(image);
+}
